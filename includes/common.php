89fbd291041c3f46a10e194e547a5b073caa7762
chjy123 <chowjy-am22@student.tarc.edu.my>
2024-05-10 22:15:16 +0800

    Update common.php

--- includes/common.php
+++ includes/common.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("t3.chaw264c6t0p.us-east-1.rds.amazonaws.com", "admin", "12345678", "ecommerce");
+$con = mysqli_connect("t4.chaw264c6t0p.us-east-1.rds.amazonaws.com", "admin", "12345678", "ecommerce");
 $con=mysqli_connect("localhost","root","","ecommerce");
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
